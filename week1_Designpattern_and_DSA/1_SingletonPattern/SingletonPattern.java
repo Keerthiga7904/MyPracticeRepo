acd09923507d766af0cdf82aed0f6f278df27135
Keerthiga7904 <keerthidilli9@gmail.com>
2025-06-19 14:37:49 +0530

    SingletonPattern.java file is added

---
+++ week1_Designpattern_and_DSA/1_SingletonPattern/SingletonPattern.java
@@ -0,0 +1,39 @@
+ class Logger {
+    private static volatile Logger instance;
+
+    private Logger() {
+
+        System.out.println("Logger initialized.");
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            synchronized (Logger.class) {
+                if (instance == null) {
+                    instance = new Logger();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("[LOG]: " + message);
+    }
+}
+
+public class SingletonPattern {
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("First message");
+        logger2.log("Second message");
+
+        if (logger1 == logger2) {
+            System.out.println("Singleton works - one logger instance.");
+        } else {
+            System.out.println("Singleton failed - different instances.");
+        }
+    }
+}
